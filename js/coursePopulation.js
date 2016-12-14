95df520809636a41c9ca5582afdf1572c3ccd5b8
rajat arora <rajat arora>
2016-12-14 13:25:19 -0500

    created a drop courses js file

--- js/coursePopulation.js
+++ js/coursePopulation.js
@@ -3,18 +3,6 @@ function finish() {
     $("#completionPopup").css("display", "block");
 }
 
-// This is when we delete a course and it will remove the classes
-function deleteCourse(classType, idType) {
-    var confirmation = confirm("are you sure you want to delete " + classType);
-    if (confirmation) {
-        $("." + classType + "C1").css("display", "none");
-        $("." + classType + "T1").css("display", "none");
-        $("." + classType + "T2").css("display", "none");
-        $("." + classType + "T3").css("display", "none");
-        $("#cc" + classType).css("display", "none");
-        $("#" + idType).css("display", "none");
-    }
-}
 
 // Adding a course from the search into the other bar
 function addCourse(classTypes, ids) {
